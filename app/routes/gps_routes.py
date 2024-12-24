764c1e313b9e04e6e053e2da31b2f8677c5be5f5
eiurrutia <eiurrutia@uc.cl>
2024-12-24 13:34:44 -0300

    fix: check manually data format

--- app/routes/gps_routes.py
+++ app/routes/gps_routes.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from app.models.gps_data import GPSPayload
 from app.services.gps_service import process_gps_data
 import os
+import json
+from pydantic import ValidationError
 
 TENANT_ID = os.getenv("TENANT_ID")
 
@@ -10,9 +12,35 @@ router = APIRouter(prefix="/webhook/gps", tags=["Webhook GPS Data"])
 
 @router.post("", include_in_schema=False)
 @router.post("/")
-async def receive_gps_data(payload: GPSPayload):
-    print("Received GPS data")
-    print(payload.dict())
+async def receive_gps_data(request: Request):
+    """
+    Endpoint to receive GPS data from Visionaline devices.
+    """
+    try:
+        content_type = request.headers.get("content-type")
+        print("Content-Type:", content_type)
+        raw_body = await request.body()
+        print("Raw body received:", raw_body)
+
+        payload_dict = json.loads(raw_body)
+        print("Decoded JSON:", payload_dict)
+
+    except json.JSONDecodeError as e:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid JSON: {e}"
+        )
+
+    try:
+        payload = GPSPayload(**payload_dict)
+    except ValidationError as ve:
+        print("Validation error:", ve)
+        raise HTTPException(
+            status_code=422,
+            detail=str(ve)
+        )
+
+    print("Received GPS data:", payload.dict())
 
     # Check type and tenant ID
     if payload.type != "GPS":
@@ -20,7 +48,7 @@ async def receive_gps_data(payload: GPSPayload):
             status_code=400,
             detail="Invalid payload type. Expected 'GPS' "
                    f"and received '{payload.type}'"
-            )
+        )
     if str(payload.tenantId) != TENANT_ID:
         raise HTTPException(status_code=403, detail="Invalid tenant ID")
 
@@ -29,4 +57,4 @@ async def receive_gps_data(payload: GPSPayload):
         print(f"Error processing GPS data: {result['message']}")
         raise HTTPException(status_code=400, detail=result["message"])
 
-    return {"status": "received", "data": payload}
+    return {"status": "received", "data": payload.dict()}
