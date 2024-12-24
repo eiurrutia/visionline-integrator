30c6c4600bf711b045dd4b51d357322cfaa37d3f
eiurrutia <eiurrutia@uc.cl>
2024-12-24 11:52:50 -0300

    feat: add tenant and type valditaion to webhook

--- app/routes/gps_routes.py
+++ app/routes/gps_routes.py
@@ -1,11 +1,36 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from app.models.gps_data import GPSPayload
+from app.services.gps_service import process_gps_data
+import os
 
-router = APIRouter(prefix="/gps", tags=["GPS Data"])
+TENANT_ID = os.getenv("TENANT_ID")
+
+router = APIRouter(prefix="/webhook/gps", tags=["Webhook GPS Data"])
 
 
-@router.post("/", include_in_schema=True)
+@router.post("", include_in_schema=False)
+@router.post("/")
 async def receive_gps_data(payload: GPSPayload):
     print("Received GPS data")
     print(payload.dict())
+
+    # Check type and tenant ID
+    if payload.type != "GPS":
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid payload type. Expected 'GPS' "
+                   f"and received '{payload.type}'"
+            )
+    if str(payload.tenantId) != TENANT_ID:
+        raise HTTPException(status_code=403, detail="Invalid tenant ID")
+
+    try:
+        await process_gps_data(payload)
+    except Exception as e:
+        print(f"Error processing GPS data: {e}")
+        raise HTTPException(
+            status_code=500, 
+            detail="Error processing GPS data"
+        )
+
     return {"status": "received", "data": payload}
