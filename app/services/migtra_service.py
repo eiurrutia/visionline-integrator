77a8c435b14c4107da889c336088183fb42e0bf3
eiurrutia <eiurrutia@uc.cl>
2024-12-28 12:00:51 -0300

    feat: migtra integration activable

--- app/services/migtra_service.py
+++ app/services/migtra_service.py
@@ -11,6 +11,8 @@ logger = logging.getLogger("apscheduler")
 MIGTRA_URL = os.getenv("MIGTRA_URL")
 MIGTRA_USERNAME = os.getenv("MIGTRA_USERNAME")
 MIGTRA_PASSWORD = os.getenv("MIGTRA_PASSWORD")
+MIGTRA_INTEGRATION_ACTIVATE = \
+    os.getenv("MIGTRA_INTEGRATION_ACTIVATE", "false").lower() == 'true'
 
 
 async def send_gps_data_to_migtra(gps_data: List[dict]) -> bool:
@@ -19,36 +21,46 @@ async def send_gps_data_to_migtra(gps_data: List[dict]) -> bool:
         transformed_data = \
             transform_gps_data_for_migtra(payload_id, gps_data)
 
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                MIGTRA_URL,
-                json=transformed_data,
-                headers={"Content-Type": "application/json"},
-                auth=(MIGTRA_USERNAME, MIGTRA_PASSWORD),
-                timeout=30
-            )
-            logger.info(f"[MIGTRA API] Response: {response.json()}")
+        if MIGTRA_INTEGRATION_ACTIVATE:
+            async with httpx.AsyncClient() as client:
+                response = await client.post(
+                    MIGTRA_URL,
+                    json=transformed_data,
+                    headers={"Content-Type": "application/json"},
+                    auth=(MIGTRA_USERNAME, MIGTRA_PASSWORD),
+                    timeout=30
+                )
+                logger.info(f"[MIGTRA API] Response: {response.json()}")
 
-        response_data = response.json()
-        if response.status_code == 200:
-            await log_migtra_integration(
-                payload_id, gps_data, response_data, "success"
-            )
-            logger.info("[MIGTRA API] Data sent successfully "
-                        f"for payload ID: {payload_id}")
+            response_data = response.json()
+            if response.status_code == 200:
+                await log_migtra_integration(
+                    payload_id, gps_data, response_data, "success"
+                )
+                logger.info("[MIGTRA API] Data sent successfully "
+                            f"for payload ID: {payload_id}")
+            else:
+                await log_migtra_integration(
+                    payload_id, gps_data, response_data, "failed"
+                )
+                logger.error("[MIGTRA API] Failed to send data. "
+                             f"Payload ID: {payload_id}, "
+                             f"Response: {response_data}")
+                return False
         else:
+            logger.info("[MIGTRA API] Integration is not activated. "
+                        "Data not sent.")
             await log_migtra_integration(
-                payload_id, gps_data, response_data, "failed"
+                payload_id, gps_data, None, "not_activated"
             )
-            logger.error("[MIGTRA API] Failed to send data. "
-                         f"Payload ID: {payload_id}, "
-                         f"Response: {response_data}")
+            return True
 
     except Exception as e:
         await log_migtra_integration(
             payload_id, gps_data, None, "failed", str(e)
         )
         logger.error(f"[MIGTRA API] Exception while sending data: {e}")
+        return False
     return True
 
 
